008fcc258d730a9eb1b10bcab7de1e076f922d93
Yochyo <herodragon25@gmail.com>
2022-02-06 15:05:47 +0100

    added docs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,10 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  host: 'localhost:8080',
-  apiUrl: `http://localhost:8080/`
+  production: true,
+  host: 'backend.yochyo.me',
+  apiUrl: `http://backend.yochyo.me:80/`
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
